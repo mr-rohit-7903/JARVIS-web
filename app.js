f1aaf4295733e430c6a076c73029f1b102d5083a
Rohit Bej <145686961+mr-rohit-7903@users.noreply.github.com>
2024-08-30 17:12:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+function speak(text) {
+    const text_speak = new SpeechSynthesisUtterance(text);
+
+    text_speak.rate = 1;
+    text_speak.volume = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {
+    var day = new Date();
+    var hour = day.getHours();
+
+    if (hour >= 0 && hour < 12) {
+        speak("Good Morning Rohit...");
+    } else if (hour >= 12 && hour < 17) {
+        speak("Good Afternoon Rohit...");
+    } else {
+        speak("Good Evening Rohit...");
+    }
+}
+
+window.addEventListener('load', () => {
+    speak("Initializing JARVIS...");
+    wishMe();
+});
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+    const currentIndex = event.resultIndex;
+    const transcript = event.results[currentIndex][0].transcript;
+    content.textContent = transcript;
+    takeCommand(transcript.toLowerCase());
+};
+
+btn.addEventListener('click', () => {
+    content.textContent = "Listening...";
+    recognition.start();
+});
+
+function takeCommand(message) {
+    if (message.includes('hey') || message.includes('hello')) {
+        speak("Hello Rohit, How May I Help You?");
+    } else if(message == ('vivek') || message == ('hardik') || message == ('sambit') || message == ('akhilesh') || message == ('om') || message == ('sombar') || message == ('ratnesh') || message == ('yash') || message == ('devanshu') || message == ('divyanshu')){
+        g1 = message + "behen ke lowde."
+        g2 = message + "ki maa ka bhosda."
+        g3 = message + "ki maa ki chhuuut. "
+        g4 = message + "madr choud hey"
+        g5 = message + "ki maa randi"
+        g6 = message + "ki maa ke raseeley, dudh"
+        g7 = message + "ki behen ki chuut mey mera lund"
+        g8 = message + "lund hey"
+        let gali = [g1, g2, g3, g4, g5, g6, g7, g8]
+        speak(gali[Math.floor(Math.random() * gali.length)])
+    }
+    else if (message.includes("open google")) {
+        window.open("https://google.com", "_blank");
+        speak("Opening Google...");
+    } else if (message.includes("open youtube")) {
+        window.open("https://youtube.com", "_blank");
+        speak("Opening Youtube...");
+    } else if (message.includes("open facebook")) {
+        window.open("https://facebook.com", "_blank");
+        speak("Opening Facebook...");
+    } else if (message.includes('what is') || message.includes('who is') || message.includes('what are')) {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "This is what I found on the internet regarding " + message;
+        speak(finalText);
+    } else if (message.includes('wikipedia')) {
+        window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "").trim()}`, "_blank");
+        const finalText = "This is what I found on Wikipedia regarding " + message;
+        speak(finalText);
+    } else if (message.includes('time')) {
+        const time = new Date().toLocaleString(undefined, { hour: "numeric", minute: "numeric" });
+        const finalText = "The current time is " + time;
+        speak(finalText);
+    } else if (message.includes('date')) {
+        const date = new Date().toLocaleString(undefined, { month: "short", day: "numeric" });
+        const finalText = "Today's date is " + date;
+        speak(finalText);
+    } else if (message.includes('calculator')) {
+        window.open('Calculator:///');
+        const finalText = "Opening Calculator";
+        speak(finalText);
+    } else {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "I found some information for " + message + " on Google";
+        speak(finalText);
+    }
+}
